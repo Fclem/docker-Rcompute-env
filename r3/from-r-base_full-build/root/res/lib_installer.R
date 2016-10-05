ced1cc47ab44c2ea1f0dc5acee881fbb16d93bfb
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-05 19:44:51 +0300

    updated

---
+++ r3/from-r-base_full-build/root/res/lib_installer.R
@@ -0,0 +1,11 @@
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = T, repos="http://cran.rstudio.com/")
+}
+
+packages <- function(file_){
+	pck_list <- unname(as.list(as.data.frame(readLines(file_, warn=F), stringsAsFactors = F))[[1]])
+	ipak(pck_list)
+	sapply(pck_list,function(x) require(x))
+}
