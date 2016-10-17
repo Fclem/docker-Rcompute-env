7fc41e6589c57d56e27cd61f35dcce50b43397f4
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-17 15:07:30 +0300

    optimized Dockerfile added viridis and tidyr libraries and fixed lib_installer function

--- _res/root_source/res/lib_installer.R
+++ _res/root_source/res/lib_installer.R
@@ -7,5 +7,6 @@ ipak <- function(pkg){
 packages <- function(file_){
 	pck_list <- unname(as.list(as.data.frame(readLines(file_, warn=F), stringsAsFactors = F))[[1]])
 	ipak(pck_list)
-	sapply(pck_list,function(x) require(x))
+	sapply(pck_list, require, character.only = T)
+	# sapply(pck_list, require)
 }
